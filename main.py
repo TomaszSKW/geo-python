04c0574cacaa05b9cfaf6e6b4229da4ac6be09f2
TomaszSKW <tomasz.skwar@onet.pl>
2023-06-09 17:18:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,37 +3,39 @@ from download_data.wczytanie import download_data_from_files, create_grouped_df,
 from charts.spektogram import create_spectogram
 from charts.periodogram import create_periodogram
 from charts.heatmapa import create_heatmap
-from charts.statystyki import*
-from charts.polar import create_polar
 from charts.colorbar import create_colorbar
+from charts.polar import create_polar
+from charts.statystyki_def import*
 
-# path_155 = "./data/gPhoneX_155_2020"
-path_157 = "./data/gphone_157_2020"
 
-# df = download_data_from_files(path_155, 61, 91)
-df = download_data_from_files(path_157, 0, 3)
+path_155 = "./data/gphone-155_2020"
+# path_157 = "./data/gphone-157_2020"
 
-df = calculate_frequency(df)
-# print(df)
+df = download_data_from_files(path_155, 0, 2)
+# df = download_data_from_files(path_157, 0, 3)
 
-print('grupowanie')
+# df = calculate_frequency(df)
+# # print(df)
+
+# print('grupowanie')
 # df_minutes = create_grouped_df(df, ['data', 'godzina', 'minuta'])
 # df_minutes['czas'] = df_minutes['data'] + pd.to_timedelta(df_minutes['godzina'], 'hour') + pd.to_timedelta(df_minutes['minuta'], 'minute')
 # df_minutes = calculate_frequency(df_minutes)
 
-# df_hours = create_grouped_df(df, ['data', 'godzina'])
-# df_hours['czas'] = df_hours['data'] + pd.to_timedelta(df_hours['godzina'], 'hour')
-# df_hours = calculate_frequency(df_hours)
+df_hours = create_grouped_df(df, ['data', 'godzina'])
+df_hours['czas'] = df_hours['data'] + pd.to_timedelta(df_hours['godzina'], 'hour')
+df_hours = calculate_frequency(df_hours)
 
 # df_days = create_grouped_df(df, ['data'])
 # df_days['czas'] = df_days['data']
-# df_days = calculate_frequency(df_days)
+#df_days = calculate_frequency(df_days)
 
 # df_month = create_grouped_df(df, ['miesiac', 'rok'])
 # df_month = calculate_frequency(df_month)
 
+
 # df.to_csv('data/df.csv', index=False)
-# df_minutes.to_csv('./data/df_minutes.csv', sep=';', decimal=',', index=False)
+#df_minutes.to_csv('./data/df_minutes.csv', sep=';', decimal=',', index=False)
 # df_hours.to_csv('./data/df_hours.csv', index=False)
 # df_days.to_csv('./data/df_days.csv', index=False)
 # df_month.to_csv('./data/df_month.csv', index=False)
@@ -41,9 +43,8 @@ print('grupowanie')
 # create_spectogram(df_minutes)
 # create_periodogram(df)
 # create_heatmap(df_minutes)
+# create_colorbar(df_hours, 31)
 # create_polar(df_hours)
-# create_colorbar(df_hours, df, 30)
-
 # calculate_mean(df['miesiac'], df['przyspieszenie'])
 # calculate_roll(df['miesiac'], df['przyspieszenie'])
 # calculate_std(df['przyspieszenie'])
